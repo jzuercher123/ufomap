9d4d06a64d5f0ee81f2fac5963a0e1f4c976347d
Joseph A. Zuercher <jzuercher09@gmail.com>
2024-06-23 19:06:03 -0500

    api issue resolved

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const csvParser = require("csv-parser");
 var port = 4444;
 var serverUrl = "127.0.0.1";
-
 var http = require("http");
 var path = require("path");
 var fs = require("fs");
-
-
-function readCSV(file) {
-    let csvData = [];
-    fs.createReadStream(file)
-        .pipe(csvParser())
-        .on('data', function (row) {
-            csvData.push(row);
-        }).on('end', function () {
-        console.log('CSV file successfully processed')
-    });
-    return csvData;
-}
 
 
 console.log("Starting web server at " + serverUrl + ":" + port);
@@ -37,9 +23,3 @@ http.createServer( function(req, res) {
     });
 }).listen(4444, '127.0.0.1')
 
-
-
-
-//geodata.forEach(row => {
-//  console.log(`City: ${row['city']}, State: ${row['state']}, Country: ${row['country']}`);
-//});
